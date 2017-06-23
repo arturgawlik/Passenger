3e71290d9e0f2f76e3792a1a603eef9fd0fc41da
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2017-06-24 00:14:08 +0200

    moustly fixed some issues with Test and EndToEndTest projects

---
+++ Passenger.Tests/Services/UserServicesTests.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Passenger.Tests.Services
+{
+    public class UserServicesTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            true.ShouldBeEquivalentTo(false);
+        }
+    }
+}
